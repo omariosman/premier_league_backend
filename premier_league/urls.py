d952773247000c89a33046ecef0b3b24e1de923f
omar <omarosman23@aucegypt.edu>
2022-04-28 23:25:40 +0200

    queires layers 1

--- premier_league/urls.py
+++ premier_league/urls.py
@@ -9,6 +9,18 @@ urlpatterns = [
     path('insert_match_review/', views.insert_match_review, name="insert_match_review"),
     path('show_match_review/', views.show_match_review, name="show_match_review"),
     path('insert_fan/', views.insert_fan, name="insert_fan"),
+    path('get_nationality/', views.get_nationality, name="get_nationality"),
+    path('get_players_with_nationality/', views.get_players_with_nationality, name="get_players_with_nationality"),
+    path('get_clubs/', views.get_clubs, name="get_clubs"),
+    path('get_club_info/', views.get_club_info, name="get_club_info"),
+    path('get_players/', views.get_players, name="get_players"),
+    path('get_player_info/', views.get_player_info, name="get_player_info"),
+    path('get_stadiums/', views.get_stadiums, name="get_stadiums"),
+    path('get_stadium_info/', views.get_stadium_info, name="get_stadium_info"),
+
+
+
+
 
 
 
--- premier_league/views.py
+++ premier_league/views.py
@@ -71,3 +71,79 @@ def insert_fan(request):
 
 
     
+
+def get_nationality(request):
+    cursor.execute("SELECT distinct nationality FROM player")
+    nationality = cursor.fetchall()
+    print(nationality)
+    return HttpResponse(json.dumps(nationality), content_type="application/json")
+
+def get_players_with_nationality(request):
+    data = JSONParser().parse(request)
+    nationality = data["nationality"]
+    nationality = data["nationality"]
+    query = """SELECT p.player_name, p.date_of_birth, p.nationality, pc.season, pc.club_name FROM premier_league.player as p join player_club_season as pc on pc.player_name = p.player_name and pc.date_of_birth = p.date_of_birth where nationality=%s;"""
+    tuple1 = (nationality,)
+    cursor.execute(query, tuple1)
+    players_with_nationality = cursor.fetchall()
+    return HttpResponse(json.dumps(players_with_nationality), content_type="application/json")
+
+
+def get_clubs(request):
+    cursor.execute("SELECT distinct club_name FROM club")
+    clubs = cursor.fetchall()
+    print(clubs)
+    return HttpResponse(json.dumps(clubs), content_type="application/json")
+
+
+def get_club_info(request):
+    data = JSONParser().parse(request)
+    club_name = data["club_name"]
+    query = """SELECT * FROM club c where c.club_name=%s;"""
+    tuple1 = (club_name,)
+    cursor.execute(query, tuple1)
+    club_info = cursor.fetchall()
+    return HttpResponse(json.dumps(club_info), content_type="application/json")
+
+
+
+
+def get_players(request):
+    cursor.execute("SELECT distinct player_name FROM player")
+    players = cursor.fetchall()
+    print(players)
+    return HttpResponse(json.dumps(players), content_type="application/json")
+
+
+def get_player_info(request):
+    data = JSONParser().parse(request)
+    player_name = data["player_name"]
+    query = """SELECT * FROM player p where p.player_name=%s;"""
+    tuple1 = (player_name,)
+    cursor.execute(query, tuple1)
+    player_info = cursor.fetchall()
+    return HttpResponse(json.dumps(player_info), content_type="application/json")
+
+
+
+
+def get_stadiums(request):
+    cursor.execute("SELECT distinct stadium_name FROM stadium")
+    stadiums = cursor.fetchall()
+    print(stadiums)
+    return HttpResponse(json.dumps(stadiums), content_type="application/json")
+
+
+
+def get_stadium_info(request):
+    data = JSONParser().parse(request)
+    stadium_name = data["stadium_name"]
+    query = """select c.club_name, s.stadium_name from club c join stadium s on s.stadium_name = c.stadium_name where s.stadium_name =%s;"""
+    tuple1 = (stadium_name,)
+    cursor.execute(query, tuple1)
+    stadium_info = cursor.fetchall()
+    return HttpResponse(json.dumps(stadium_info), content_type="application/json")
+
+
+
+
